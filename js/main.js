0bfcc9731c32b5802219ce46f7c4dc0184285f0d
GuillaumeM59 <manier-guillaume@orange.fr>
2015-11-22 18:36:42 +0100

    fish home content: video background, menu @media +medium, form bar for search travel with jquery datepicker template.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+jQuery(function($){
+  $('#datetimepicker').datepicker({
+    dateFormat : 'dd/mm/yy',
+    minDate : 0,
+    maxDate : 365,
 
+  }
+  );
+});
+
+jQuery(function($){
+  $('#datetimepicker2').datepicker({
+    dateFormat : 'dd/mm/yy',
+    minDate : 1,
+    maxDate : 365,
+
+  }
+  );
+});
