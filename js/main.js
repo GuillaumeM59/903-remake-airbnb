e4c81022e33804024e02df9aa97a719a6a78db11
GuillaumeM59 <manier-guillaume@orange.fr>
2015-11-23 15:20:43 +0100

    add HOST centered part and second for small devices

--- js/main.js
+++ js/main.js
@@ -18,6 +18,13 @@ jQuery(function($){
   );
 });
 
+(document).foundation({
+equalizer : {
+// Specify if Equalizer should make elements equal height once they become stacked.
+equalize_on_stack: true
+}
+});
+
 
 // CROSSFADER
 
