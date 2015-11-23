104948fa726d3982821809e6b2bf8a59b2c6c773
GuillaumeM59 <manier-guillaume@orange.fr>
2015-11-23 11:36:20 +0100

    home-onvideo id style changing, center text home to middle verticaly

--- js/main.js
+++ js/main.js
@@ -17,3 +17,21 @@ jQuery(function($){
   }
   );
 });
+
+
+// CROSSFADER
+
+// function cycleImages(){
+//       var $active = $('#imghost .active');
+//       var $next = ($active.next().length > 0) ? $active.next() : $('#imghost img:first');
+//       $next.css('z-index',2);//move the next image up the pile
+//       $active.fadeOut(1500,function(){//fade out the top image
+// 	  $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+//           $next.css('z-index',3).addClass('active');//make the next image the top one
+//       });
+//     }
+
+$(document).ready(function(){
+// run every 7s
+setInterval('cycleImages()', 7000);
+})
